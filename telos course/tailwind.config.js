77a170171a6f3a5d9d49e5d211204b8883b20771
Mars-company <voduyphong.2304@gmail.com>
2024-11-26 15:33:56 +0700

    nov-26-company

--- telos course/tailwind.config.js
+++ telos course/tailwind.config.js
@@ -1,10 +1,27 @@
 module.exports = {
   content: [
-    "./*.html"
+    "./*.html",
+    "./*/*.html",
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+
+      colors: {
+        primary: "#fc9003",
+      },
+
+      fontSize: {
+        h2: ["48px", "64px"],
+        h5: ["24px", "32px"],
+        lead1: ["18px","32px"],
+        body: ["16px", "26px"],
+
+
+      } 
+      
+    },
   },
   variants: {
     extend: {},
